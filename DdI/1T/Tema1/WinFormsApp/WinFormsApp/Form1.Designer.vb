f649b3402d1456bf41896b8edc70c30fbd062961
Pablo <instypablo@gmail.com>
2022-09-22 16:37:19 +0200

    Primer commit Ddi/1Tema/SegundoEjercicio

--- DdI/1T/Tema1/WinFormsApp/WinFormsApp/Form1.Designer.vb
+++ DdI/1T/Tema1/WinFormsApp/WinFormsApp/Form1.Designer.vb
@@ -22,10 +22,38 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(368, 304)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "&Color"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
 End Class
--- DdI/1T/Tema1/WinFormsApp/WinFormsApp/Form1.vb
+++ DdI/1T/Tema1/WinFormsApp/WinFormsApp/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
-
+    Dim backc As New Random
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.BackColor = Color.FromArgb(255, backc.Next(255), backc.Next(255), backc.Next(255))
+    End Sub
 End Class
